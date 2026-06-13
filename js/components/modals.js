b966ff52dd0757e4f07ef7afae1575ad9dd61b92
dilovantprk <dilovanre@gmail.com>
2026-06-13 22:43:17 +0300

    fix: correct SVG viewBox to 64x64 for getAuraSVGIcon to prevent modal icon alignment issues

--- js/components/modals.js
+++ js/components/modals.js
@@ -538,7 +538,7 @@ function getAuraSVGIcon(type) {
   if (iconMap[type]) {
     const innerContent = iconMap[type];
     return `
-      <svg width="64" height="64" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <svg width="64" height="64" viewBox="0 0 64 64" fill="none" xmlns="http://www.w3.org/2000/svg">
         <defs>
           <filter id="auraGlow" x="-20%" y="-20%" width="140%" height="140%">
             <feGaussianBlur stdDeviation="3" result="blur" />
